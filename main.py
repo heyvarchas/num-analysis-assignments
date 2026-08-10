c8a25ba77aa9ecce745d9a5093d8b5fd22762aa3
Varchas Jasti <jastivarchas@gmail.com>
2026-08-10 11:53:34 +0530

    modify main file for helpers and pretty print

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
+# I'm going to start the file by importing the necessary libraries and functions from other files. 
+# I will import the format_matrix and print_matrix functions from helpers.py, and other functions from their respective files.
+
+from helpers import format_matrix, print_matrix
 from gauss_jordan import gauss_jordan
-
-"""
-So basically I'm getting a floating point problem when I run the code, it displays -0.0 instead of 0.0 in the output. 
-This is a common issue with floating point arithmetic in Python (and many other programming languages) due to how numbers are represented in memory.
-So I'm going to create another function that can modify my output the way I want it to be.
-"""
-
-def clean_matrix(matrix, eps=1e-9):
-    return [
-        [0.0 if abs(x) < eps else x for x in row]
-        for row in matrix
-    ]
+from gauss_elimination import gauss_elimination
 
 def main():
+    # I want to create a menu for the user to choose which method they want to use, so I'm doing this below
+    menu = ["1. Gauss Jordan Elimination", "2. Gauss Elimination"]
+
+    # Now I take the number as input and then perform stuff accordingly
+    print("See the list of options below and choose the one you want to use:")
+    for option in menu:
+        print(option)
+    choice = input("Enter your choice: ")
+
     mat = [
         [2, 1, -1, 8],
         [-3, -1, 2, -11],
         [-2, 1, 2, -3]
     ]
 
-    result = gauss_jordan(mat)
-    result = clean_matrix(result)
-
-    print("RREF:")
-    for row in result:
-        print(row)
+    if choice == "1":
+        result = gauss_jordan(mat)
+        result = format_matrix(result)  # Format the matrix values
+        print("RREF:")
+        print_matrix(result)
+    elif choice == "2":
+        A = [[2, 1, -1],
+         [-3, -1, 2],
+         [-2, 1, 2]]
+        b = [8, -11, -3]
+        result = gauss_elimination(A, b)
+        result = format_matrix(result)
+        print("Solution:")
+        print_matrix(result)
+    else:
+        print("Invalid choice.")
+        return
 
 
 if __name__ == "__main__":
